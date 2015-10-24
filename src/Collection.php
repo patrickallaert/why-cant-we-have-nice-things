b589887531087d4f8a407fe4aec3166592946486
Maxime Fabre <ehtnam6@gmail.com>
2015-10-24 02:59:57 +0200

    Fix average function
    Signed-off-by: Maxime Fabre <ehtnam6@gmail.com>

--- src/Collection.php
+++ src/Collection.php
@@ -3,6 +3,23 @@ namespace History;
 
 class Collection extends \Illuminate\Database\Eloquent\Collection
 {
+    /**
+     * @return float|int
+     */
+    public function average($key = null)
+    {
+        if (!$this->items) {
+            return 0;
+        }
+
+        return array_sum($this->items) / count($this->items);
+    }
+
+    /**
+     * @param string|callable $groupBy
+     *
+     * @return static
+     */
     public function groupByCounts($groupBy)
     {
         return $this->groupBy($groupBy)->map(function ($entries) {
