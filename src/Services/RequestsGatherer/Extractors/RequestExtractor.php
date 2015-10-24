43924ff3c2a529b900535ef3aeaf1381c2a406ec
Maxime Fabre <ehtnam6@gmail.com>
2015-10-24 20:44:30 +0200

    Split QuestionsExtractor
    Signed-off-by: Maxime Fabre <ehtnam6@gmail.com>

--- src/Services/RequestsGatherer/Extractors/RequestExtractor.php
+++ src/Services/RequestsGatherer/Extractors/RequestExtractor.php
@@ -2,9 +2,7 @@
 namespace History\Services\RequestsGatherer\Extractors;
 
 use DateTime;
-use DateTimeZone;
 use History\Services\RequestsGatherer\ExtractorInterface;
-use Symfony\Component\DomCrawler\Crawler;
 
 class RequestExtractor extends AbstractExtractor implements ExtractorInterface
 {
@@ -15,10 +13,17 @@ class RequestExtractor extends AbstractExtractor implements ExtractorInterface
      */
     public function extract()
     {
+        // Extract Request informations
         $name               = $this->getRequestName();
         $majorityConditions = $this->getMajorityConditions();
         $timestamp          = $this->getRequestTimestamp();
-        $questions          = $this->getQuestions();
+
+        // Extract Questions
+        $questions = $this->crawler->filter('table.inline')->each(function ($question) {
+            $extractor = new QuestionsExtractor($question);
+
+            return $extractor->extract();
+        });
 
         return [
             'name'      => $name,
@@ -84,97 +89,5 @@ class RequestExtractor extends AbstractExtractor implements ExtractorInterface
                 return $text;
             }
         }
-    }
-
-    /**
-     * Get the questions asked on this RFC.
-     *
-     * @return array
-     */
-    protected function getQuestions()
-    {
-        return $this->crawler->filter('table.inline')->each(function ($question) {
-            $name    = $this->extractText('tr:first-child', $question);
-            $choices = $this->getChoices($question);
-
-            return [
-                'name'    => $name,
-                'choices' => count($choices),
-                'votes'   => $this->getVotes($question, $choices),
-            ];
-        });
-    }
-
-    /**
-     * Get the choices available for this question.
-     *
-     * @param Crawler $question
-     *
-     * @return array
-     */
-    protected function getChoices(Crawler $question)
-    {
-        return $question->filter('tr.row1 td')->each(function ($choice) {
-            return $choice->text();
-        });
-    }
-
-    /**
-     * Get the votes for a question.
-     *
-     * @param Crawler $question
-     * @param array   $choices
-     *
-     * @return array
-     */
-    protected function getVotes(Crawler $question, array $choices)
-    {
-        $votes = [];
-
-        $question
-            ->filter('tr')
-            ->reduce(function ($vote) {
-                return $vote->filter('td.rightalign a')->count() > 0;
-            })->each(function ($vote) use (&$votes, $choices) {
-                $user = $vote->filter('td.rightalign a')->text();
-
-                // Get which choice the user picked
-                $voted = 0;
-                $time  = new DateTime();
-                $vote->filter('td')->each(function ($choice, $key) use (&$voted, &$time) {
-                    $image = $choice->filter('img');
-                    if ($image->count()) {
-                        $timestamp = $image->attr('title');
-
-                        $voted = $key;
-                        $time  = $timestamp;
-                    }
-                });
-
-                // Save vote for this request
-                $votes[] = [
-                    'user_id'    => $this->replaceFullnamesByUsernames($user),
-                    'choice'     => $voted,
-                    'created_at' => DateTime::createFromFormat('Y/m/d H:i', $time, new DateTimeZone('UTC')),
-                ];
-            });
-
-        return $votes;
-    }
-
-    /**
-     * Replace the full names by usernames for
-     * the people who voted with their full names
-     *
-     * @param string $name
-     *
-     * @return string
-     */
-    protected function replaceFullnamesByUsernames($name)
-    {
-        return strtr($name, [
-            'Barry Carlyon' => 'bcarlyon',
-            'Ivan Enderlin' => 'hywan',
-        ]);
     }
 }
