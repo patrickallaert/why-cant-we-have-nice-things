b1eeb656243d8e82789684836796c62717d63a61
Maxime Fabre <ehtnam6@gmail.com>
2015-10-26 23:30:00 +0100

    RFCs might also contain C

--- resources/assets/js/components/highlight.js
+++ resources/assets/js/components/highlight.js
@@ -1,6 +1,7 @@
 import 'highlight.js/styles/github.css';
 import Highlight from 'highlight.js/lib/highlight';
-import php from 'highlight.js/lib/languages/php';
+import cpp from 'highlight.js/lib/languages/cpp';
 
+Highlight.registerLanguage('cpp', cpp);
 Highlight.registerLanguage('php', php);
 Highlight.initHighlightingOnLoad();
