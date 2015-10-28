295d8e3381aa4c17a84670a4d17a5fb747be69e2
Maxime Fabre <ehtnam6@gmail.com>
2015-10-29 00:39:03 +0100

    Group request chunk

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -6,14 +6,9 @@ import '../sass/styles.scss';
 import './components/tables';
 import './components/tooltips';
 
-if (document.querySelector('pre code')) {
-    require.ensure([], () => {
-        require('./components/highlight');
-    });
-}
-
 if (document.querySelector('.request')) {
     require.ensure([], () => {
-        require('./components/request-navigation');
+        require('./components/highlight');
+        require('./components/comments-folding');
     });
 }
