37461b3c59bf3afcca3a75e371a26651c0b20309
Maxime Fabre <ehtnam6@gmail.com>
2015-10-29 23:39:57 +0100

    Close #15 - Only require needed hljs languages

--- resources/assets/js/components/highlight.js
+++ resources/assets/js/components/highlight.js
@@ -1,4 +1,12 @@
 import 'highlight.js/styles/github.css';
-import Highlight from 'highlight.js';
+import Highlight from 'highlight.js/lib/highlight.js';
+
+// Import languages commonly used in RFCs
+Highlight.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'));
+Highlight.registerLanguage('diff', require('highlight.js/lib/languages/diff'));
+Highlight.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+Highlight.registerLanguage('json', require('highlight.js/lib/languages/json'));
+Highlight.registerLanguage('php', require('highlight.js/lib/languages/php'));
+Highlight.registerLanguage('python', require('highlight.js/lib/languages/python'));
 
 Highlight.initHighlightingOnLoad();
