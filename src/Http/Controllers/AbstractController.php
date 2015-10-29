2851ea334d47663adcef2cb6133ad1db6182a8d5
Maxime Fabre <ehtnam6@gmail.com>
2015-10-29 22:12:59 +0100

    Fix call to ParameterBag

--- src/Http/Controllers/AbstractController.php
+++ src/Http/Controllers/AbstractController.php
@@ -1,6 +1,10 @@
 <?php
 namespace History\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Fluent;
+use Psr\Http\Message\ServerRequestInterface;
 use Twig_Environment;
 
 abstract class AbstractController
@@ -16,5 +20,22 @@ abstract class AbstractController
     public function __construct(Twig_Environment $views)
     {
         $this->views = $views;
+    }
+
+    /**
+     * @param Builder                $query
+     * @param ServerRequestInterface $request
+     * @param int                    $perPage
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    protected function paginate($query, ServerRequestInterface $request, $perPage = 50)
+    {
+        /** @var Paginator $paginator */
+        $parameters = new Fluent($request->getQueryParams());
+        $paginator  = $query->paginate($perPage, ['*'], 'page', $parameters->page);
+        $paginator->setPath($request->getUri()->getPath());
+
+        return $paginator;
     }
 }
--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -2,9 +2,7 @@
 namespace History\Http\Controllers;
 
 use History\Entities\Models\Event;
-use Illuminate\Pagination\Paginator;
 use Psr\Http\Message\ServerRequestInterface;
-use Symfony\Component\HttpFoundation\ParameterBag;
 
 class EventsController extends AbstractController
 {
@@ -15,16 +13,8 @@ class EventsController extends AbstractController
      */
     public function index(ServerRequestInterface $request)
     {
-        /* @var Paginator $events */
-        $parameters = new ParameterBag($request->getQueryParams());
-        $events     = Event::with('eventable.question.request', 'eventable.user')
-                       ->latest()
-                       ->paginate(
-                               50, ['*'], 'page',
-                               $parameters->get('page')
-                           );
-
-        $events->setPath($request->getUri()->getPath());
+        $events = Event::with('eventable.question.request', 'eventable.user')->latest();
+        $events = $this->paginate($events, $request);
 
         return $this->views->render('events/index.twig', [
             'events' => $events,
