1de0d6cce95dc93499c859086fc7dc86ffd089fc
Maxime Fabre <ehtnam6@gmail.com>
2015-10-30 21:34:49 +0100

    Close #45 - Use Redis for cache

--- src/Providers/CacheServiceProvider.php
+++ src/Providers/CacheServiceProvider.php
@@ -2,9 +2,11 @@
 namespace History\Providers;
 
 use Illuminate\Cache\FileStore;
+use Illuminate\Cache\RedisStore;
 use Illuminate\Cache\Repository as IlluminateCache;
 use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Redis\Database;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class CacheServiceProvider extends AbstractServiceProvider
@@ -23,8 +25,26 @@ class CacheServiceProvider extends AbstractServiceProvider
      */
     public function register()
     {
+        $this->container->share(FileStore::class, function () {
+            return new FileStore(new Filesystem(), $this->container->get('paths.cache'));
+        });
+
+        $this->container->share(RedisStore::class, function () {
+            $redis = new Database([
+                'cluster' => false,
+                'default' => [
+                    'host'     => '127.0.0.1',
+                    'port'     => 6379,
+                    'database' => 0,
+                ],
+            ]);
+
+            return new RedisStore($redis);
+        });
+
         $this->container->share(Repository::class, function () {
-            $store = new FileStore(new Filesystem(), $this->container->get('paths.cache'));
+            $store = getenv('CACHE_DRIVER') === 'redis' ? RedisStore::class : FileStore::class;
+            $store = $this->container->get($store);
 
             return new IlluminateCache($store);
         });
